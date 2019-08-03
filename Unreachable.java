872310eef7f7f750b044358ec0f6e86752edeb0e
Piyush Warke <44377484+piyush19198@users.noreply.github.com>
2019-08-03 22:09:21 +0530

    Create Unreachable.java

---
+++ Unreachable.java
@@ -0,0 +1,24 @@
+public class loop{
+	public static void main(String[] args){
+		/*
+		while(i==1){
+			
+		}
+		do{
+			
+		}while(false);
+		*/
+		System.out.println("Byte: "+Byte.SIZE+" : "+Byte.MIN_VALUE" : "+Byte.MAX_VALUE);
+		System.out.println("Short: "+Short.SIZE+" : "+Short.MIN_VALUE" : "+Short.MAX_VALUE);
+		System.out.println("Long: "+Long.SIZE+" : "+Long.MIN_VALUE" : "+Long.MAX_VALUE);
+		System.out.println("Integer: "+Integer.SIZE+" : "+Integer.MIN_VALUE" : "+Integer.MAX_VALUE);
+		System.out.println("Float: "+Float.SIZE+" : "+Float.MIN_VALUE" : "+Float.MAX_VALUE);
+		System.out.println("Double: "+Double.SIZE+" : "+Double.MIN_VALUE" : "+Double.MAX_VALUE);
+		System.out.println("Character: "+Character.SIZE+" : "+Character.MIN_VALUE" : "+Character.MAX_VALUE);
+		
+		//for(;;){
+		//System.out.println("For Loop");
+		//}
+		}
+	}
+}
